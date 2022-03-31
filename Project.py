205e6631e2eac21cd27f620b6b363d1b51430c42
Thibauld DEFFONTAINES <thibaulddef41@gmail.com>
2022-03-31 21:03:46 +0200

    Initialisation

---
+++ Project.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import *
+
+
+class Astre(self, Nom=None, Taille=None, Orbite=None):
+	self.Nom = Nom
+	self.Taille = Taille
+	self.orbite = Orbite
+
+def set_Nom(self, nouveau_Nom):
+self.Nom = nouveau_Nom	
+
+	def set_Orbite(self, nouvelle_Taille):
+self.Taille = nouvelle_Taille
+
+	def set_Orbite(self, nouvelle_Orbite):
+self.Orbite = nouvelle_Orbite
+
+def get_Nom(self):
+return self.Nom	
+
+	def get_Taille(self):
+return self.Taille
+
+	def get_Orbite(self):
+return self.Orbite
+	
+
+
+def main():
+	espace = tk.Tk()
+jeu.geometry("1080x720")
+jeu.minsize(720, 480)
+jeu.config(background='black')
+app = Fenetre_principale(jeu)
+jeu.mainloop()
+
